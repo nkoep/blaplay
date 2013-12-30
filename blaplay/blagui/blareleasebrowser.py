dfc9a73136fbf9ff511503556f671b64daa11a0a
Niklas Koep <niklas.koep@gmail.com>
2013-12-30 17:03:11 +0100

    blareleasesbrowser.py: PEP8-compliance changes
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- blaplay/blagui/blareleasebrowser.py
+++ blaplay/blagui/blareleasebrowser.py
@@ -59,7 +59,8 @@ class BlaCellRendererPixbuf(blaguiutils.BlaCellRendererBase):
         if isinstance(content, str):
             layout = self.__get_layout(widget)
             height = layout.get_pixel_size()[-1]
-        else: height = IMAGE_SIZE
+        else:
+            height = IMAGE_SIZE
         return (0, 0, IMAGE_SIZE, height)
 
     def on_render(self, window, widget, background_area, cell_area,
@@ -67,7 +68,7 @@ class BlaCellRendererPixbuf(blaguiutils.BlaCellRendererBase):
         cr = window.cairo_create()
         content = self.get_property("content")
         if isinstance(content, str):
-            # render active resp. inactive rows
+            # Render active resp. inactive rows.
             layout = self.__get_layout(widget)
             layout.set_width((expose_area.width + expose_area.x) * pango.SCALE)
             layout.set_ellipsize(pango.ELLIPSIZE_END)
@@ -93,16 +94,15 @@ class BlaCellRendererPixbuf(blaguiutils.BlaCellRendererBase):
                     math.ceil(expose_area.y + size[1] / 2) + 0.5)
             cr.stroke()
         else:
-            if not content: return
+            if not content:
+                return
             cr.set_source_pixbuf(content, expose_area.x, expose_area.y)
             cr.rectangle(*expose_area)
             cr.fill()
 
-gobject.type_register(BlaCellRendererPixbuf)
-
 class BlaRelease(object):
     __EMPTY_PIXBUF = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, True, 8,
-                IMAGE_SIZE, IMAGE_SIZE)
+                                    IMAGE_SIZE, IMAGE_SIZE)
     __EMPTY_PIXBUF.fill(0)
 
     def __init__(self, raw):
@@ -123,35 +123,31 @@ class BlaRelease(object):
         return datetime.date(*self.date[:3]).isocalendar()[:2]
 
     def get_cover(self, restore=False):
-        import gobject
-
         pixbuf = path = None
-        image_base = os.path.join(blaconst.RELEASES, ("%s-%s" % (
-                    self.artist_name, self.release_name)).replace(
-                    " ", "_")
-            )
+        path = ("%s-%s" % (self.artist_name, self.release_name)).replace(
+            " ", "_")
+        image_base = os.path.join(blaconst.RELEASES, path)
         for ext in ["jpg", "png"]:
             try:
                 path = "%s.%s" % (image_base, ext)
                 pixbuf = gtk.gdk.pixbuf_new_from_file(path)
-            except gobject.GError: pass
+            except gobject.GError:
+                pass
             else:
                 pixbuf = pixbuf.scale_simple(IMAGE_SIZE, IMAGE_SIZE,
-                        gtk.gdk.INTERP_HYPER)
+                                             gtk.gdk.INTERP_HYPER)
                 break
         else:
             if not restore:
                 url = blafm.get_image_url(self.__raw["image"])
                 try:
                     image, message = urllib.urlretrieve(url)
-                    path = "%s.%s" % (
-                            image_base, blautil.get_extension(image))
+                    path = "%s.%s" % (image_base, blautil.get_extension(image))
                     shutil.move(image, path)
-                    pixbuf = gtk.gdk.pixbuf_new_from_file(
-                            path).scale_simple(IMAGE_SIZE, IMAGE_SIZE,
-                            gtk.gdk.INTERP_HYPER
-                    )
-                except (IOError, gobject.GError): pass
+                    pixbuf = gtk.gdk.pixbuf_new_from_file(path).scale_simple(
+                        IMAGE_SIZE, IMAGE_SIZE, gtk.gdk.INTERP_HYPER)
+                except (IOError, gobject.GError):
+                    pass
 
         self.cover = pixbuf or BlaRelease.__EMPTY_PIXBUF
         return path
@@ -170,7 +166,7 @@ class BlaReleaseBrowser(BlaScrolledWindow):
         vbox = gtk.VBox()
         vbox.set_border_width(10)
 
-        # heading
+        # Heading
         hbox = gtk.HBox()
         items = [
             ("<b><span size=\"xx-large\">New releases</span></b>",
@@ -191,7 +187,7 @@ class BlaReleaseBrowser(BlaScrolledWindow):
         hbox.pack_start(alignment, expand=False)
         vbox.pack_start(hbox, expand=False)
 
-        # type selector
+        # Type selector
         self.__hbox = gtk.HBox(spacing=5)
         self.__hbox.set_border_width(10)
         items = [
@@ -203,32 +199,37 @@ class BlaReleaseBrowser(BlaScrolledWindow):
         radiobutton = None
         for label, filt in items:
             radiobutton = gtk.RadioButton(radiobutton, label)
-            if filt == active: radiobutton.set_active(True)
+            if filt == active:
+                radiobutton.set_active(True)
             radiobutton.connect("toggled", self.__filter_changed, filt)
             self.__hbox.pack_start(radiobutton, expand=False)
         button = gtk.Button("Refresh")
         button.set_focus_on_click(False)
         button.connect_object(
-                "clicked", BlaReleaseBrowser.__update_models, self)
+            "clicked", BlaReleaseBrowser.__update_models, self)
         self.__hbox.pack_start(button, expand=False, padding=5)
         vbox.pack_start(self.__hbox, expand=False)
 
-        # releases list
+        # Releases list
         def cell_data_func_pixbuf(column, renderer, model, iterator):
             release = model[iterator][0]
-            try: renderer.set_property("content", release.cover)
-            except AttributeError: renderer.set_property("content", release)
+            try:
+                renderer.set_property("content", release.cover)
+            except AttributeError:
+                renderer.set_property("content", release)
 
         def cell_data_func_text(column, renderer, model, iterator):
             release = model[iterator][0]
             try:
-                markup = "<b>%s</b>\n%s\nReleased: %s" % (release.release_name,
-                        release.artist_name, release.release_date)
-            except AttributeError: markup = ""
+                markup = "<b>%s</b>\n%s\nReleased: %s" % (
+                    release.release_name, release.artist_name,
+                    release.release_date)
+            except AttributeError:
+                markup = ""
             renderer.set_property("markup", markup.replace("&", "&amp;"))
 
         self.__treeview = blaguiutils.BlaTreeViewBase(
-                set_button_event_handlers=False)
+            set_button_event_handlers=False)
         self.__treeview.set_rules_hint(True)
         self.__treeview.get_selection().set_mode(gtk.SELECTION_SINGLE)
         self.__treeview.set_headers_visible(False)
@@ -244,7 +245,7 @@ class BlaReleaseBrowser(BlaScrolledWindow):
         self.__treeview.append_column(column)
         self.__treeview.connect("row_activated", self.__row_activated)
         self.__treeview.connect(
-                "button_press_event", self.__button_press_event)
+            "button_press_event", self.__button_press_event)
         self.__models = map(gtk.ListStore, [gobject.TYPE_PYOBJECT] * 2)
         self.__treeview.set_model(self.__models[active])
         vbox.pack_start(self.__treeview, expand=True, padding=10)
@@ -271,12 +272,14 @@ class BlaReleaseBrowser(BlaScrolledWindow):
 
         with self.__lock:
             images = set()
-            # TODO: these don't always seem to timeout properly
+            # FIXME: These don't always seem to timeout properly.
             releases = (blafm.get_new_releases(),
-                    blafm.get_new_releases(recommended=True))
+                        blafm.get_new_releases(recommended=True))
             active = blacfg.getint("general", "releases.filter")
-            if releases[0]: self.__count_library = len(releases[0])
-            if releases[1]: self.__count_recommended = len(releases[1])
+            if releases[0]:
+                self.__count_library = len(releases[0])
+            if releases[1]:
+                self.__count_recommended = len(releases[1])
             items = [
                 (blaconst.NEW_RELEASES_FROM_LIBRARY, releases[0] or []),
                 (blaconst.NEW_RELEASES_RECOMMENDED, releases[1] or [])
@@ -292,56 +295,65 @@ class BlaReleaseBrowser(BlaScrolledWindow):
                         previous_week = week
                         date = self.__cw_to_start_end_day(*week)
                         date = "%s - %s" % (date[0].strftime("%a %d %b"),
-                                date[1].strftime("%a %d %b"))
+                                            date[1].strftime("%a %d %b"))
                         datestring = "\n<span size=\"larger\"><b>%s\n"
                         if week == current_week:
                             datestring %= "This week</b></span> (%s)" % date
-                        else: datestring %= "Week of %s</b></span>" % date
+                        else:
+                            datestring %= "Week of %s</b></span>" % date
                         model.append([datestring])
                     model.append([release])
                     path = release.get_cover()
-                    if path: images.add(path)
+                    if path:
+                        images.add(path)
                 self.__models[filt] = model
 
-            # get rid of covers for releases that don't show up anymore
+            # Get rid of covers for releases that don't show up anymore.
             for image in set(blautil.discover(blaconst.RELEASES)).difference(
-                    images):
-                try: os.unlink(image)
-                except OSError: pass
+                images):
+                try:
+                    os.unlink(image)
+                except OSError:
+                    pass
 
             gobject.idle_add(set_sensitive, True)
             gobject.idle_add(self.__treeview.set_model, self.__models[active])
 
             if active == blaconst.NEW_RELEASES_FROM_LIBRARY:
                 count = self.__count_library
-            else: count = self.__count_recommended
+            else:
+                count = self.__count_recommended
             gobject.idle_add(
-                    self.emit, "count_changed", blaconst.VIEW_RELEASES, count)
+                self.emit, "count_changed", blaconst.VIEW_RELEASES, count)
             return True
 
     def __filter_changed(self, radiobutton, filt):
-        # the signal of the new active radiobutton arrives last so only change
-        # the config then
+        # The signal of the new active radiobutton arrives last so only change
+        # the config then.
         if radiobutton.get_active():
             blacfg.set("general", "releases.filter", filt)
             self.__treeview.set_model(self.__models[filt])
             if filt == blaconst.NEW_RELEASES_FROM_LIBRARY:
                 count = self.__count_library
-            else: count = self.__count_recommended
+            else:
+                count = self.__count_recommended
             self.emit("count_changed", blaconst.VIEW_RELEASES, count)
 
     def __row_activated(self, treeview, path, column):
         blautil.open_url(treeview.get_model()[path][0].release_url)
 
     def __button_press_event(self, treeview, event):
-        try: path = treeview.get_path_at_pos(*map(int, [event.x, event.y]))[0]
-        except TypeError: return False
+        try:
+            path = treeview.get_path_at_pos(*map(int, [event.x, event.y]))[0]
+        except TypeError:
+            return False
 
         model = treeview.get_model()
         release = model[path][0]
-        if not isinstance(release, BlaRelease): return True
-        if event.button in [1, 2]:
-            if event.type in [gtk.gdk._2BUTTON_PRESS, gtk.gdk._3BUTTON_PRESS]:
+        if not isinstance(release, BlaRelease):
+            return True
+        if event.button in (1, 2):
+            if event.type in (gtk.gdk._2BUTTON_PRESS, gtk.gdk._3BUTTON_PRESS):
                 return True
             return False
 
@@ -360,7 +372,7 @@ class BlaReleaseBrowser(BlaScrolledWindow):
                 "http://www.last.fm/user/%s/library/music/%s" %
                 (user, artist_history_url))
             items.append(("View artist history",
-                    lambda *x: blautil.open_url(artist_history_url)))
+                          lambda *x: blautil.open_url(artist_history_url)))
 
         menu = gtk.Menu()
         for label, callback in items:
@@ -373,7 +385,7 @@ class BlaReleaseBrowser(BlaScrolledWindow):
         return False
 
     def __cw_to_start_end_day(self, year, week):
-        # FIXME: this is erroneous
+        # FIXME: This is not working as it's supposed to.
         date = datetime.date(year, 1, 1)
         timedelta = datetime.timedelta(days=(week-1)*7+1)
         return date + timedelta, date + timedelta + datetime.timedelta(days=6)
@@ -382,17 +394,18 @@ class BlaReleaseBrowser(BlaScrolledWindow):
         releases = blautil.deserialize_from_file(blaconst.RELEASES_PATH)
         if releases:
             model = gtk.ListStore(gobject.TYPE_PYOBJECT)
-            # pixbufs aren't initialized when they're unpickled so we need to
-            # instantiate them while restoring. to speed up restoring we force
-            # the use of possibly cached images by passing True as `restore'
-            # kwarg
+            # Pixbufs aren't initialized when they're unpickled so we need to
+            # instantiate them while restoring. To speed up restoring we force
+            # the use of possibly cached images by passing `restore=True'.
             for release in releases:
-                try: release.get_cover(restore=True)
-                except AttributeError: pass
+                try:
+                    release.get_cover(restore=True)
+                except AttributeError:
+                    pass
             [model.append([release]) for release in releases]
             self.__treeview.set_model(model)
 
-        # check for new releases now and every two hours
+        # Check for new releases now and every two hours
         self.__update_models()
         gobject.timeout_add(2 * 3600 * 1000, self.__update_models)
 
