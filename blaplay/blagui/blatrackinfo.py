nkoep/blaplay

commit aae986eed90b17f6effdd58d2758e4e6e431ee02

Hide track info widget when the player is stopped
Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

blaplay/blagui/blatrackinfo.py
@@ -153,9 +153,10 @@ class _CoverDisplay(gtk.DrawingArea):
                 gobject.source_remove(self._fetch_timeout_id)
             self._fetch_timeout_id = gobject.timeout_add( 250, fetch_cover)
 
-class BlaTrackInfo(gtk.Viewport):
+class BlaTrackInfo(gtk.Viewport, blautil.BlaInitiallyHidden):
     def __init__(self, metadata_fetcher):
         super(BlaTrackInfo, self).__init__()
+        blautil.BlaInitiallyHidden.__init__(self)
         self.set_shadow_type(gtk.SHADOW_IN)
 
         self._track = None
@@ -220,10 +221,12 @@ class BlaTrackInfo(gtk.Viewport):
             return
         state = player.get_state()
 
-        if state == blaconst.STATE_STOPPED:
+        player_stopped = state == blaconst.STATE_STOPPED
+        if player_stopped:
             self._track = None
         else:
             self._track = track
+        self.set_visible(player_stopped)
         self._cover_display.update_cover(self._track)
         self._update_track_info(self._track)
 

blaplay/blautil/__init__.py
@@ -477,3 +477,21 @@ class BlaFrozenDict(dict):
             raise ValueError("Entry for key '%s' already exists" % key)
         super(BlaFrozenDict, self).__setitem__(key, value)
 
+class BlaInitiallyHidden(object):
+    """
+    Class hides  guarantees
+    """
+
+    def __new__(cls):
+        if not gtk.Widget in cls.__bases__:
+            raise TypeError("Class must have gtk.Widget in its bases")
+        return super(BlaInitiallyHidden, cls).__new__(cls)
+
+    def __init__(self):
+        self._callback_id = self.connect_object(
+            "map", BlaInitiallyHidden._on_map, self)
+
+    def _on_map(self):
+        self.disconnect(self._callback_id)
+        self.set_visible(False)
+

commit 709fa229ce50b61c7d57d2b624bf08d39035f9ef

Fix silly bug introduced in aae986e
Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

blaplay/blagui/blatrackinfo.py
@@ -226,7 +226,7 @@ class BlaTrackInfo(gtk.Viewport, blautil.BlaInitiallyHidden):
             self._track = None
         else:
             self._track = track
-        self.set_visible(player_stopped)
+        self.set_visible(not player_stopped)
         self._cover_display.update_cover(self._track)
         self._update_track_info(self._track)
 